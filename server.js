6c0f789c24ad492b5de1fcfbfed65eadc1fee17a
belugaman9981 <littledage@outlook.com>
2026-06-13 22:28:43 -0700

    Update server.js

--- server.js
+++ server.js
@@ -932,7 +932,7 @@ app.get('/sitemap.xml', (req, res) => {
       loc: `${base}/?article=${encodeURIComponent(a.headline||'')}`,
       priority: '0.6',
       changefreq: 'daily' 
-    })) 
+    }))
   ];
   const xml = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
